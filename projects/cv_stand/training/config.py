e8042d519ea3f9ac15bc7309ae20d019d223b950
N-31V <litvintseva.ann@gmail.com>
2023-04-20 16:32:49 +0300

    update svd scripts

--- projects/cv_stand/training/config.py
+++ projects/cv_stand/training/config.py
@@ -2,11 +2,11 @@ from typing import Optional, Tuple
 import torch
 from torchvision.transforms import Compose, ToTensor, Resize
 from torchvision.models.detection.ssdlite import ssdlite320_mobilenet_v3_large
-from torchvision.models.detection.faster_rcnn import fasterrcnn_resnet50_fpn_v2
+from torchvision.models.detection.faster_rcnn import fasterrcnn_resnet50_fpn
 from torch.optim.lr_scheduler import StepLR
 from torch.utils.data import DataLoader, random_split
-from core.architecture.experiment.nn_experimenter import ObjectDetectionExperimenter, FitParameters
-from core.architecture.datasets.object_detection_datasets import YOLODataset
+from fedot_ind.core.architecture.experiment.nn_experimenter import ObjectDetectionExperimenter, FitParameters
+from fedot_ind.core.architecture.datasets.object_detection_datasets import YOLODataset
 
 
 def get_dataset(
@@ -38,7 +38,7 @@ def fasterrcnn_task(train_ds, val_ds, dl_params, ds_name):
     def f_task(device):
         return {
             'exp': ObjectDetectionExperimenter(
-                model=fasterrcnn_resnet50_fpn_v2(num_classes=2),
+                model=fasterrcnn_resnet50_fpn(num_classes=2),
                 device=device
             ),
             'fit_params': FitParameters(
@@ -76,7 +76,6 @@ def ssd_task(train_ds, val_ds, dl_params, ds_name):
                 train_dl=DataLoader(train_ds, shuffle=True, **dl_params),
                 val_dl=DataLoader(val_ds, **dl_params),
                 num_epochs=300,
-                optimizer_params={'lr': 0.005},
                 lr_scheduler=StepLR,
                 lr_scheduler_params={'step_size': 10, 'gamma': 0.5},
             ),
@@ -96,8 +95,7 @@ def ssd_task(train_ds, val_ds, dl_params, ds_name):
 
 TASKS = {
     'ssd': ssd_task(*get_dataset(batch_size=64), ds_name='binary_small'),
-    'rcnn_big': fasterrcnn_task(*get_dataset(batch_size=8, resize=(922, 1228)), ds_name='binary_big'),
-    'rcnn_small': fasterrcnn_task(*get_dataset(batch_size=8, resize=(320, 320)), ds_name='binary_small')
+    'rcnn': fasterrcnn_task(*get_dataset(batch_size=12, resize=(922, 1228)), ds_name='binary_big'),
 }
 
 
